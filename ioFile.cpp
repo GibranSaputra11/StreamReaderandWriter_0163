ef00012751c515f2920f491a8b35c2ed641156b1
GibranSaputra11 <gibranbantul@gmail.com>
2025-06-09 14:38:01 +0700

    add perintah menutup file

--- ioFile.cpp
+++ ioFile.cpp
@@ -23,4 +23,6 @@ int main(){
         // Menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
 }
